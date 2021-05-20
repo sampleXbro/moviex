af0db441a7f4b217518b9757a907bb6ac43dada3
Sergey Zhabskiy <zhabskiy.s.lightit@gmail.com>
2021-05-20 09:52:08 +0300

    Init commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+import '../styles/globals.sass'
+import {Provider} from 'react-redux'
+import store from '../redux/configStore'
+
+
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <Provider store={store}>
+    <Component {...pageProps} />
+    </Provider>
+  )
+}
+
+export default MyApp
