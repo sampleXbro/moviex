37b8cda7f02d1e03c7ee4348f5c2c6efbfa47352
Sergey Zhabskiy <zhabskiy.s.lightit@gmail.com>
2021-05-25 10:52:06 +0300

    add(now-playing.tsx)

---
+++ pages/now-playing.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function NowPlaying() {
+  return (
+    <>
+      <Head>
+        <title>Moviex | Now playing</title>
+      </Head>
+      <div>NOW PLAYING</div>
+    </>
+  )
+}
