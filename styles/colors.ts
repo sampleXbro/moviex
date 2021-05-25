f8ad63e58f7866658dfac23db754925dd5494007
Sergey Zhabskiy <zhabskiy.s.lightit@gmail.com>
2021-05-25 10:42:43 +0300

    add colors.ts

---
+++ styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  'primary-color-dark': '#F57C00',
+  'primary-color': '#FF9800',
+  'primary-color-light': '#FFE0B2',
+  'primary-color-text': '#212121',
+  'accent-color': '#536DFE',
+  'primary-text-color': '#212121',
+  'secondary-text-color': '#757575',
+  'divider-color': '#BDBDBD',
+  'background-color': '#CFD8DC',
+}
