92193da42a9354f712dab1fbee67ec370f346bee
Sergey Zhabskiy <zhabskiy.s.lightit@gmail.com>
2021-05-31 20:52:45 +0300

    add favorites get set

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -4,13 +4,21 @@ import { useMovie } from '../../redux/selectors/selectors'
 import { getMovieApi, getMovieVideosApi } from '../../components/api/api'
 import { Box, Divider, Paper, Typography } from '@material-ui/core'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import axios, { AxiosResponse } from 'axios'
 import ReactPlayer from 'react-player/youtube'
 import { withAuthCheck } from '../../components/HOCs/withAuthCheck'
+import { Star, StarBorder } from '@material-ui/icons'
 
 function MoviePage(): JSX.Element {
   const { data, videos } = useMovie()
+
+  const favorites: Array<string> = JSON.parse(
+    localStorage.getItem('moviex/favorites') || '[]'
+  )
+  const isFavorite = favorites.includes(String(data.id))
+
+  const [favorite, setFavorite] = useState(isFavorite)
   const genres: string = data.genres.map((g) => g.name).join(', ')
 
   const renderVideos = (): JSX.Element[] => {
@@ -26,6 +34,22 @@ function MoviePage(): JSX.Element {
         <ReactPlayer url={`https://www.youtube.com/watch?v=${vid.key}`} />
       </Box>
     ))
+  }
+
+  const handleFavoriteClick = (): void => {
+    if (isFavorite) {
+      localStorage.setItem(
+        'moviex/favorites',
+        JSON.stringify(
+          favorites.filter((fav: string) => Number(fav) !== data.id)
+        )
+      )
+      setFavorite(false)
+    } else {
+      favorites.push(String(data.id))
+      localStorage.setItem('moviex/favorites', JSON.stringify(favorites))
+      setFavorite(true)
+    }
   }
 
   return (
@@ -53,7 +77,28 @@ function MoviePage(): JSX.Element {
             justifyContent={'space-between'}
             alignItems={'flex-end'}
           >
-            <Typography variant={'h4'}>{data.title}</Typography>
+            <Box
+              display={'flex'}
+              justifyContent={'space-between'}
+              width={'100%'}
+              alignItems={'center'}
+            >
+              <Typography variant={'h4'}>{data.title}</Typography>
+              {favorite ? (
+                <Star
+                  color={'primary'}
+                  fontSize={'large'}
+                  cursor={'pointer'}
+                  onClick={handleFavoriteClick}
+                />
+              ) : (
+                <StarBorder
+                  fontSize={'large'}
+                  cursor={'pointer'}
+                  onClick={handleFavoriteClick}
+                />
+              )}
+            </Box>
           </Box>
           <Divider
             color={'black'}
