44ee84491a4f4dccdf3d53450c0c68ca6ae6bfbf
Sergey Zhabskiy <zhabskiy.s.lightit@gmail.com>
2021-06-08 10:47:17 +0300

    fix login in firefox

--- src/common/components/hooks/useToken.ts
+++ src/common/components/hooks/useToken.ts
@@ -1,10 +1,14 @@
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 import { useAuth } from '../../selectors/selectors'
+dayjs.extend(utc)
 
 export const useToken = () => {
   const { token } = useAuth()
 
-  const isValidToken: boolean = dayjs(token.expires_at).diff(dayjs()) > 0
+  const utcDateExpires = token.expires_at.split(' ').slice(0, -1).join(' ')
+
+  const isValidToken: boolean = dayjs.utc(utcDateExpires).diff(dayjs.utc()) > 0
 
   return { isValidToken, token }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,16 @@ import { wrapper } from '../app/store'
 import React from 'react'
 import { MuiThemeProvider } from '@material-ui/core'
 import theme from '../common/styles/theme'
-import App, { AppInitialProps } from 'next/app'
+import { AppProps } from 'next/app'
 
 import MainLayout from '../common/components/common/MainLayout'
 
-class MyApp extends App<AppInitialProps> {
-  public render() {
-    const { Component, pageProps } = this.props
-    return (
-      <MuiThemeProvider theme={theme}>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </MuiThemeProvider>
-    )
-  }
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <MuiThemeProvider theme={theme}>
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  </MuiThemeProvider>
+)
 
 export default wrapper.withRedux(MyApp)
