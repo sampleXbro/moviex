356460b1f07cd9e12b3b74545805216fbf47fe61
Zhabskiy Sergey <zhabskiy.s.lightit@gmail.com>
2021-09-16 12:33:13 +0300

    feat: add tests for singleMovieSagas

---
+++ src/__mocks__/mocks.ts
@@ -0,0 +1,113 @@
+export const mockMovie = {
+  adult: false,
+  backdrop_path: '/620hnMVLu6RSZW6a5rwO8gqpt0t.jpg',
+  belongs_to_collection: null,
+  budget: 0,
+  genres: [
+    {
+      id: 16,
+      name: 'Animation',
+    },
+    {
+      id: 35,
+      name: 'Comedy',
+    },
+    {
+      id: 10751,
+      name: 'Family',
+    },
+    {
+      id: 14,
+      name: 'Fantasy',
+    },
+  ],
+  homepage: 'https://www.disneyplus.com/movies/luca/7K1HyQ6Hl16P',
+  id: 508943,
+  imdb_id: 'tt12801262',
+  original_language: 'en',
+  original_title: 'Luca',
+  overview:
+    'Luca and his best friend Alberto experience an unforgettable summer on the Italian Riviera. But all the fun is threatened by a deeply-held secret: they are sea monsters from another world just below the water’s surface.',
+  popularity: 1046.376,
+  poster_path: '/jTswp6KyDYKtvC52GbHagrZbGvD.jpg',
+  production_companies: [
+    {
+      id: 3,
+      logo_path: '/1TjvGVDMYsj6JBxOAkUHpPEwLf7.png',
+      name: 'Pixar',
+      origin_country: 'US',
+    },
+    {
+      id: 2,
+      logo_path: '/wdrCwmRnLFJhEoH8GSfymY85KHT.png',
+      name: 'Walt Disney Pictures',
+      origin_country: 'US',
+    },
+  ],
+  production_countries: [
+    {
+      iso_3166_1: 'US',
+      name: 'United States of America',
+    },
+  ],
+  release_date: '2021-06-17',
+  revenue: 38000000,
+  runtime: 95,
+  spoken_languages: [
+    {
+      english_name: 'English',
+      iso_639_1: 'en',
+      name: 'English',
+    },
+    {
+      english_name: 'Italian',
+      iso_639_1: 'it',
+      name: 'Italiano',
+    },
+  ],
+  status: 'Released',
+  tagline: '',
+  title: 'Luca',
+  video: false,
+  vote_average: 8.1,
+  vote_count: 4457,
+}
+
+export const mockVideosArray = [
+  {
+    iso_639_1: 'en',
+    iso_3166_1: 'US',
+    name: "Friendship Featurette | Disney and Pixar's Luca | Disney+",
+    key: 'N6DY_IkzPCI',
+    site: 'YouTube',
+    size: 1080,
+    type: 'Featurette',
+    official: true,
+    published_at: '2021-05-20T20:01:52.000Z',
+    id: '60f348d9f5f1c5005cb9bc3e',
+  },
+  {
+    iso_639_1: 'en',
+    iso_3166_1: 'US',
+    name: 'Official Trailer',
+    key: 'mYfJxlgR2jw',
+    site: 'YouTube',
+    size: 1080,
+    type: 'Trailer',
+    official: true,
+    published_at: '2021-04-28T13:00:15.000Z',
+    id: '6089ecab43250f00296ea152',
+  },
+  {
+    iso_639_1: 'en',
+    iso_3166_1: 'US',
+    name: 'Teaser Trailer',
+    key: 'YdAIBlPVe9s',
+    site: 'YouTube',
+    size: 1080,
+    type: 'Trailer',
+    official: true,
+    published_at: '2021-02-25T14:00:16.000Z',
+    id: '6037aeb915959f003e87c31e',
+  },
+]
