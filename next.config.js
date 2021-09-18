802a79fd3b6cca86b6c4d4539766231cc1f2f648
Zhabskiy Sergey <zhabskiy.s.lightit@gmail.com>
2021-09-18 15:48:52 +0300

    fix: change redirect

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/movies/now-playing/1',
+        destination: '/movies/popular/1',
         permanent: true,
       },
     ]
